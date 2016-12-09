f7442b88452db5375157f3c63143f87b3fb7120f
jql <978926107@qq.com>
2016-12-09 20:20:40 +0800

    增加oath

---
+++ HM2/HM2/Classes/Oauth/HMOauthViewController.h
@@ -0,0 +1,13 @@
+//
+//  HMOauthViewController.h
+//  HM2
+//
+//  Created by 焦起龙 on 16/12/9.
+//  Copyright © 2016年 JqlLove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMOauthViewController : UIViewController
+
+@end
---
+++ HM2/HM2/Classes/Oauth/HMOauthViewController.m
@@ -0,0 +1,37 @@
+//
+//  HMOauthViewController.m
+//  HM2
+//
+//  Created by 焦起龙 on 16/12/9.
+//  Copyright © 2016年 JqlLove. All rights reserved.
+//
+
+#import "HMOauthViewController.h"
+
+@interface HMOauthViewController ()
+
+@end
+
+@implementation HMOauthViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
